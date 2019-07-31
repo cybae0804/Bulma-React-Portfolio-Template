a4f47775928e43173c7d7e6b0e8673f89b57792a
Cy Bae <cy.b@mov-ology.com>
2019-07-31 14:29:53 -0700

    Added profile picture section to about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,10 +11,15 @@ class About extends Component {
           <div className="hero-body">
             <div className="container">
               <h1 className="title">{`About ${about.firstName}`}</h1>
-              <div className="content">
-                {
-                  about.longDesc.map((paragraph, index) => <p key={index}>{paragraph}</p>)
-                }
+              <div className="content columns">
+                <div className="column is-one-third">
+                  <img src="https://picsum.photos/600/500" alt="Profile" />
+                </div>
+                <div className="column is-two-thirds longDescription">
+                  {
+                    about.longDesc.map((paragraph, index) => <p key={index}>{paragraph}</p>)
+                  }
+                </div>
               </div>
             </div>
           </div>
